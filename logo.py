8606b53350f25a5bf763f57507725e84591ecda0
neotheicebird <neotheicebird@gmail.com>
2014-08-12 16:02:15 +0530

    Added animate logo class and playing around

--- logo.py
+++ logo.py
@@ -1,6 +1,8 @@
 """Animate a moving polygon similar to fast codesign logo"""
 import random
 import itertools
+from matplotlib import pyplot as plt
+from matplotlib import animation
 
 NUM_VERTICES = 4
 
@@ -19,7 +21,9 @@ class polygon3D(object):
     def __init__(self):
         self.vertices = self.random_vertices(NUM_VERTICES)
 
-    #def angular_move(self, rzx = 0, rxy = 0):
+    def angular_move(self, rzx = 0, rxy = 0):
+        """ create a rotation matrix and use the angle/frame found to rotate each point"""
+
 
 class map_to_2D(object):
     """gets a frame of the 3D object and projects it on to a 2D screen.
@@ -39,7 +43,13 @@ class map_to_2D(object):
 
 
     def map_to_xy(self):
-        vertices_on_xy = [(x, y) for x, y, z in self.polygon.vertices]
+        """instead of vertices, the edges points-pairs has to be mapped"""
+        self.polygon.edges = self.find_edges()
+        vertices_on_xy = [((x1, y1), (x2, y2)) for (x1, y1, z1), (x2, y2, z2) in self.polygon.edges]
+        #print "Vertices on XY plane: ", vertices_on_xy
+        return vertices_on_xy
+
+
 
 
 class Animate_logo(object):
@@ -47,6 +57,31 @@ class Animate_logo(object):
     def __init__(self):
         self.animated = map_to_2D()
         print "Len: ", len(self.animated.find_edges()), self.animated.find_edges()
+        print "Len: ", len(self.animated.map_to_xy()), self.animated.map_to_xy()
+        self.init_canvas()  # Initialize the canvas where we are going to animate
+
+    def init_canvas(self):
+        self.fig = plt.figure()
+        self.ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
+        self.line, = self.ax.plot([], [], lw=2)
+        self.line.set_data([], [])
+
+    def animate(self):
+        """
+        anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=100, interval=20, blit=True)
+
+        # save the animation as an mp4.  This requires ffmpeg or mencoder to be
+        # installed.  The extra_args ensure that the x264 codec is used, so that
+        # the video can be embedded in html5.  You may need to adjust this for
+        # your system: for more information, see
+        # http://matplotlib.sourceforge.net/api/animation_api.html
+        anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+
+        plt.show()
+
+        """
+        pass
 
 
 def main():
